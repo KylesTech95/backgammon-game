4ed767c35b9456f476d27ca8165a8d20fe32035e
kylestech95 <kylestech95@outlook.com>
2024-01-26 08:29:33 -0500

    feat: setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let triArr = document.querySelectorAll('.tri');
-
-//change triangle colors by mod
-triArr.forEach((tri,i)=>i%2!==0 ? tri.style = `border-bottom: 18rem solid grey;` : tri.style = `border-bottom: 18rem solid brown;`)
+const gray = `border-bottom: 18rem solid grey;`
+const brown = `border-bottom: 18rem solid brown;`
+//change triangle colors with modulo
+triArr.forEach((tri,i)=>i%2!==0 ? tri.style = gray : tri.style = brown)
